046c293a5d94326fe52a8c82c57198cce5d907e6
fsancheztemprano <fsancheztemprano@danielcastelao.org>
2020-01-23 12:34:00 +0100

    www tut mysql multi insert,prepared statement, query

---
+++ ej_09/ej_www/MySQLi/mysql_6a_prepared_statement.php
@@ -0,0 +1,53 @@
+<?php
+$servername = "localhost";
+$username = "narf";
+$password = "narf";
+$dbname = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// prepare and bind
+$stmt = $conn->prepare("INSERT INTO MyGuests (firstname, lastname, email) VALUES (?, ?, ?)");
+$stmt->bind_param("sss", $firstname, $lastname, $email);
+
+/*
+  This function binds the parameters to the SQL query and tells the database what the parameters are. The "sss" argument lists the types of data that the parameters are. The s character tells mysql that the parameter is a string.
+
+The argument may be one of four types:
+
+i - integer
+d - double
+s - string
+b - BLOB
+We must have one of these for each parameter.
+
+By telling mysql what type of data to expect, we minimize the risk of SQL injections.
+*/
+
+// set parameters and execute
+$firstname = "John";
+$lastname = "Doe";
+$email = "john@example.com";
+$stmt->execute();
+
+$firstname = "Mary";
+$lastname = "Moe";
+$email = "mary@example.com";
+$stmt->execute();
+
+$firstname = "Julie";
+$lastname = "Dooley";
+$email = "julie@example.com";
+$stmt->execute();
+
+echo "New records created successfully";
+
+$stmt->close();
+$conn->close();
+?>
